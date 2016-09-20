5c5859c7c55ab0e2f933098916bae82b9d716d30
kimjunghak <kboy1025@gmail.com>
2016-09-21 03:15:19 +0900

    change Array -> ArrayList, add BinaryInsertionSort

--- sort/src/input.java
+++ sort/src/input.java
@@ -8,45 +8,111 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class input {
-    public static void main(String args[]) throws IOException {
-        FileInputStream input = new FileInputStream("C:/Users/KJH/IdeaProjects/sort/src/data02.txt");
 
+    public void insertionReadNWrite() throws IOException{
+        ArrayList arrList = readFile();
+
+        insertionSort(arrList);
+
+        FileOutputStream output = new FileOutputStream("C:/Users/KJH/IdeaProjects/sort/src/hw02_00_201203406_insertion.txt");
+        writeFile(arrList, output);
+    }
+
+    public void binaryInsertionReadNWrite() throws IOException{
+        ArrayList arrList = readFile();
+
+        insertionSortWithBSearch(arrList);
+
+        FileOutputStream output = new FileOutputStream("C:/Users/KJH/IdeaProjects/sort/src/hw02_00_201203406_binary_insertion.txt");
+        writeFile(arrList, output);
+    }
+
+    public void mergeReadNWrite() throws IOException{
+        ArrayList arrList = readFile();
+
+        mergeSort(arrList);
+
+        FileOutputStream output = new FileOutputStream("C:/Users/KJH/IdeaProjects/sort/src/hw02_00_201203406_merge.txt");
+        writeFile(arrList, output);
+    }
+
+    public void merge3_WayReadNWrite() throws IOException{
+        ArrayList arrList = readFile();
+
+        mergeSort3_Way(arrList);
+
+        FileOutputStream output = new FileOutputStream("C:/Users/KJH/IdeaProjects/sort/src/hw02_00_201203406_3-way_merge.txt");
+        writeFile(arrList, output);
+    }
+
+    private ArrayList readFile() throws IOException {
+        FileInputStream input = new FileInputStream("C:/Users/KJH/IdeaProjects/sort/src/data02.txt");
         StringBuffer buffer = new StringBuffer();
-        int[] arr = new int[1024];
-        int data, index = 0, cost = 0;
-        String write;
+        ArrayList arrList = new ArrayList();
+        int data;
 
         while( (data = input.read()) != -1){
             if( data == ',') {
-                arr[index++] = Integer.parseInt(buffer.toString());
+                arrList.add(Integer.parseInt(buffer.toString()));
                 buffer.setLength(0);
             }
             else
                 buffer.append((char)data);
-            cost++;
         }
-        insertionSort(arr);
+        arrList.add(Integer.parseInt(buffer.toString()));
+        return arrList;
+    }
 
-        FileOutputStream output = new FileOutputStream("C:/Users/KJH/IdeaProjects/sort/src/hw02_00_201203406_insertion.txt");
-        for(int loop=0; loop<arr.length; loop++) {
-            write = arr[loop] + ",";
+    private void writeFile(ArrayList arrList, FileOutputStream output) throws IOException {
+        String write;
+        for(int loop = 0; loop<arrList.size(); loop++) {
+            write = arrList.get(loop).toString() + ",";
             output.write(write.getBytes());
         }
-        write = arr[arr.length-1]+"\r\n";
+        write = arrList.get(arrList.size()-1).toString()+"\r\n";
         output.write(write.getBytes());
         output.close();
     }
 
-    static void insertionSort(int[] data){
+    private void insertionSort(ArrayList arrList){
         int temp, pos;
-        for(int i=1 ; i<data.length ; i++){
-            temp = data[i];
+        for(int i=1 ; i<arrList.size() ; i++){
+            temp = Integer.parseInt(arrList.get(i).toString());
             pos = i;
-            while(pos > 0 && temp < data[pos-1]){
-                data[pos] = data[pos-1];
+            while(pos > 0 && temp < Integer.parseInt(arrList.get(pos-1).toString())){
+                arrList.set(pos, Integer.parseInt(arrList.get(pos-1).toString()));
                 pos = pos-1;
             }
-            data[pos] = temp;
+            arrList.set(pos, temp);
         }
+    }
+
+    private void insertionSortWithBSearch(ArrayList arrList){
+        int temp, left, right, middle;
+
+        for(int i=1 ; i<arrList.size() ; i++) {
+            temp = Integer.parseInt(arrList.get(i).toString());
+            left = 0;
+            right = i;
+            while(left < right){
+                middle = (left + right) / 2;
+                if(temp >= Integer.parseInt(arrList.get(middle).toString()))
+                    left = middle + 1;
+                else
+                    right = middle;
+            }
+            for(int j=i ; j>left ; j--) {
+                arrList.set(j, Integer.parseInt(arrList.get(j-1).toString()));
+                arrList.set(j-1, temp);
+            }
+        }
+    }
+
+    private void mergeSort(ArrayList arrList){
+
+    }
+
+    private void mergeSort3_Way(ArrayList arrList){
+
     }
 }
---
+++ sort/src/test.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+/**
+ * Created by KJH on 2016-09-20.
+ */
+public class test {
+    public static void main(String args[]) throws IOException{
+        input input = new input();
+        input.insertionReadNWrite();
+        input.binaryInsertionReadNWrite();
+        input.mergeReadNWrite();
+    }
+}
